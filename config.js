ec8382bacc39c6be22e89d7960ba4cad13250002
fuking12 <pqthumchinthaka105@gmail.com>
2025-05-22 05:39:46 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Bot Settings
+  SESSION_ID: "my-bot-session", // ඔබට අවශ්‍ය නම් වෙනස් කරන්න
+  BOT_NAME: "RDB_BOT",
+  
+  // Connection Options
+  MULTI_DEVICE: true,
+  QR_TIMEOUT: 60, // තත්පර 60ක් QR රැඳී සිටී
+  
+  // Owner
+  OWNER_NUMBER: "94766863255"
+};
